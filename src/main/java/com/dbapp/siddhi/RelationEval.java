5ef247226203ec23b6e655c611653126a80b8512
tttMelody <245915794@qq.com>
2016-12-26 16:00:19 +0800

    完成基础关联的功能测试

--- src/main/java/com/dbapp/siddhi/RelationEval.java
+++ src/main/java/com/dbapp/siddhi/RelationEval.java
@@ -19,21 +19,21 @@ public class RelationEval {
                 //"define stream condition2Stream ( catBehavior string, catOutcome string, srcAddress string, deviceCat string, srcUsername string, catObject string, destAddress string, appProtocol string ); " +
                 "" +
                 "@info(name = 'condition1') " +
-                "from rawStream[ catBehavior == '/Authentication/Verify' and catOutcome == 'FAIL' and not( srcAddress is null ) ]#window.time(1 min) " +
+                "from rawStream[ catBehavior == '/Authentication/Verify' and catOutcome == 'FAIL' and not( srcAddress is null ) ]#window.time(10 sec) " +
                 "select srcAddress, deviceCat, srcUsername, destAddress, appProtocol " +//soc join部分要的字段取出来
-                "insert into e1_OutputStream;" +
+                "insert current events into e1_OutputStream;" +
                 "" +
                 "@info(name = 'condition2') " +
-                "from rawStream[ catBehavior == '/Authentication/Verify' and catOutcome == 'OK' and not( srcAddress is null ) ]#window.time(1 min)  " +
+                "from rawStream[ catBehavior == '/Authentication/Verify' and catOutcome == 'OK' and not( srcAddress is null ) ]#window.time(10 sec)  " +
                 "select srcAddress, deviceCat, srcUsername, destAddress, appProtocol " +
-                "insert into e2_OutputStream;"
+                "insert current events into e2_OutputStream;"
                 + "" +
                 "@info(name = 'result') " +
-                "from every ( e1 = e1_OutputStream<9:> ) -> e2 = e2_OutputStream[ e1.srcAddress == srcAddress " +
+                "from every ( e1 = e1_OutputStream<9:> ) -> every ( e2 = e2_OutputStream[ e1.srcAddress == srcAddress " +
                                                                          "and e1.deviceCat == deviceCat " +
                                                                          "and e1.srcUsername == srcUsername " +
                                                                          "and e1.destAddress == destAddress " +
-                                                                         "and e1.appProtocol == appProtocol ] "+
+                                                                         "and e1.appProtocol == appProtocol ] ) "+
                 "within 1 second " +
                 "select 'relationEvent' as event, e1.srcAddress, e1.deviceCat, e1.srcUsername, e1.destAddress, e1.appProtocol " +
                 "insert into resultOutputStream;"
@@ -65,18 +65,18 @@ public class RelationEval {
         executionPlanRuntime.start();
 
         //Sending events to Siddhi
-        //for ( int i = 0 ; i < 9 ; i++ ) {
-        //    rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" ,"deviceCat", "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
-        //}
-        //rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "OK" , "1.1.1.1" ,"deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
+        for ( int i = 0 ; i < 9 ; i++ ) {
+            rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" ,"deviceCat", "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
+        }
+        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "OK" , "1.1.1.1" ,"deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
         //
-        //Thread.sleep( 1000*20 );
+        Thread.sleep( 1000*20 );
 
         for ( int i = 0 ; i < 9 ; i++ ) {
             rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" ,"deviceCat", "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
             //Thread.sleep( 1000*2 );
         }
-        //Thread.sleep( 1000*2 );
+        //Thread.sleep( 1000*2 );实践指出这个time gap是两个事件之间的时间
         rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "OK" , "1.1.1.1" ,"deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
         //Thread.sleep( 500 );
 
