d917bc0c25dfd8df1d079adab2bf107bd229944e
tttMelody <245915794@qq.com>
2016-12-27 16:59:05 +0800

    完成基础关联的功能测试

--- src/main/java/com/dbapp/siddhi/RelationEval.java
+++ src/main/java/com/dbapp/siddhi/RelationEval.java
@@ -3,8 +3,10 @@ package com.dbapp.siddhi;
 import org.wso2.siddhi.core.ExecutionPlanRuntime;
 import org.wso2.siddhi.core.SiddhiManager;
 import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.query.output.callback.QueryCallback;
 import org.wso2.siddhi.core.stream.input.InputHandler;
 import org.wso2.siddhi.core.stream.output.StreamCallback;
+import org.wso2.siddhi.core.util.EventPrinter;
 
 /**
  * Created by AH on 2016/12/21.
@@ -25,7 +27,7 @@ public class RelationEval {
                 "insert into e1_OutputStream;" +
                 "" +
                 "@info(name = 'condition2') " +
-                "from rawStream[ catBehavior == '/Authentication/Verify' and catOutcome == 'OK' and not( srcAddress is null ) ]#window.time(10 sec)  " +
+                "from rawStream[ catBehavior == '/Authentication/Verify' and catOutcome == 'OK' and not( srcAddress is null ) ]#window.timeBatch(20 sec) " +
                 "select srcAddress, catOutcome,  deviceCat, srcUsername, destAddress, appProtocol " +
                 "insert current events into e2_OutputStream;"
                 + "" +
@@ -35,7 +37,7 @@ public class RelationEval {
                                                                          "and e1.srcUsername == srcUsername " +
                                                                          "and e1.destAddress == destAddress " +
                                                                          "and e1.appProtocol == appProtocol ] ) "+
-                "within 1 second " +//每个事件之间的间隔
+                //"within 1 second " +//每个事件之间的间隔
                 "select 'relationEvent' as event, e1.srcAddress, e1.deviceCat, e1.srcUsername, e1.destAddress, e1.appProtocol " +
                 "insert into resultOutputStream;"
                 ;
@@ -53,19 +55,28 @@ public class RelationEval {
             }
         } );
 
-        //executionPlanRuntime.addCallback( "result", new QueryCallback() {
-        //    @Override
-        //    public void receive ( long timeStamp, Event[] inEvents, Event[] removeEvents ) {
-        //        EventPrinter.print( timeStamp, inEvents, removeEvents );
-        //    }
-        //} );
+        executionPlanRuntime.addCallback( "e2_OutputStream", new StreamCallback() {
+            @Override
+            public void receive ( Event[] events ) {
+                for ( Event event : events ) {
+                    System.out.println( event.toString() );
+                }
+            }
+        } );
+
+        executionPlanRuntime.addCallback( "result", new QueryCallback() {
+            @Override
+            public void receive ( long timeStamp, Event[] inEvents, Event[] removeEvents ) {
+                EventPrinter.print( timeStamp, inEvents, removeEvents );
+            }
+        } );
 
         InputHandler rawStreamHandler = executionPlanRuntime.getInputHandler( "rawStream" );
         executionPlanRuntime.start();
         //catBehavior, catOutcome, srcAddress, deviceCat, srcUsername, catObject, destAddress, appProtocol ;group by srcAddress, srcUsername, destAddress, appProtocol
-        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
-        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
-        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
+        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject1" , "destAddress" , "appProtocol" } );
+        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject2" , "destAddress" , "appProtocol" } );
+        rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject3" , "destAddress" , "appProtocol" } );
         rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
         rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
         rawStreamHandler.send( new Object[] { "/Authentication/Verify" , "FAIL" , "1.1.1.1" , "deviceCat" , "srcUsername" , "catObject" , "destAddress" , "appProtocol" } );
